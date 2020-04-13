a06796108a449bf17222c337677520af0df025a1
George <george.camilar@eggs.de>
2020-04-13 11:52:01 +0300

    Payment Repository Test

---
+++ PizzaShop/src/test/java/pizzashop/repository/PaymentRepositoryTest.java
@@ -0,0 +1,38 @@
+package pizzashop.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pizzashop.model.Payment;
+
+import static pizzashop.model.PaymentType.Cash;
+
+public class PaymentRepositoryTest {
+
+    private static PaymentRepository repository;
+
+    @BeforeAll
+    static void initialize() {
+        repository = new PaymentRepository();
+    }
+
+    @Test
+    void testGetAll() {
+        Assertions.assertNotEquals(repository.getAll(), null);
+        Assertions.assertNotEquals(repository.getAll().size(), 0);
+    }
+
+    @Test
+    void getPayments() {
+    }
+
+    @Test
+    void addPayment() {
+        Integer initialSize = repository.getAll().size();
+        Payment payment = new Payment(4, Cash, 15d);
+        repository.add(payment);
+        Assertions.assertNotEquals(repository.getAll().size(), initialSize);
+        Assertions.assertEquals(repository.getAll().get(repository.getAll().size() - 1), payment);
+    }
+
+}
