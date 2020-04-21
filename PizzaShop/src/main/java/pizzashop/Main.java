4fe7d3750ab2e1317bc4dabb6788b4bc9a32014f
George <george.camilar@eggs.de>
2020-04-21 19:57:50 +0300

    change the main class

--- PizzaShop/src/main/java/pizzashop/Main.java
+++ PizzaShop/src/main/java/pizzashop/Main.java
@@ -21,10 +21,10 @@ import java.util.Optional;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
-        MenuRepository repoMenu=new MenuRepository();
-        PaymentRepository payRepo= new PaymentRepository();
+        MenuRepository repoMenu = new MenuRepository();
+        PaymentRepository payRepo = new PaymentRepository();
         PizzaService service = new PizzaService(repoMenu, payRepo);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainFXML.fxml"));
@@ -40,18 +40,17 @@ public class Main extends Application {
             public void handle(WindowEvent event) {
                 Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION, "Would you like to exit the Main window?", ButtonType.YES, ButtonType.NO);
                 Optional<ButtonType> result = exitAlert.showAndWait();
-                if (result.get() == ButtonType.YES){
+                if (result.get() == ButtonType.YES) {
                     //Stage stage = (Stage) this.getScene().getWindow();
-                    System.out.println("Incasari cash: "+service.getTotalAmount(PaymentType.Cash));
-                    System.out.println("Incasari card: "+service.getTotalAmount(PaymentType.Card));
+                    System.out.println("Incasari cash: " + service.getTotalAmount(PaymentType.Cash));
+                    System.out.println("Incasari card: " + service.getTotalAmount(PaymentType.Card));
 
                     primaryStage.close();
                 }
                 // consume event
-                else if (result.get() == ButtonType.NO){
+                else if (result.get() == ButtonType.NO) {
                     event.consume();
-                }
-                else {
+                } else {
                     event.consume();
 
                 }
@@ -64,6 +63,7 @@ public class Main extends Application {
         kitchenGUI.KitchenGUI();
     }
 
-    public static void main(String[] args) { launch(args);
+    public static void main(String[] args) {
+        launch(args);
     }
 }
